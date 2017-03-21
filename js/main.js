ca235b0c4a6d9951f670d615cddb22ce3a77c45d
Khanh Mai <Khanh@ECALC02SF06AGY25.local>
2017-03-21 17:38:34 -0400

    Fibonacci Sequence Dojo

--- js/main.js
+++ js/main.js
@@ -1,45 +1,17 @@
-/*for (var i = 1; i <= 100; i++) {
-    document.write("<br>You Chose " + i );
+function fibonacci() {
+
+  var number = document.querySelector('.sequence').value;
+
+var fibArray = [0, 1];
+
+for (var i = 2; i <= number; i++) {
+
+var addFib = fibArray[i-2] + fibArray[i-1];
+ fibArray.push(addFib);
+ var fibArrayAsString = fibArray.join(', ');
+
 }
 
-var randomNumber = prompt();*/
+document.querySelector('.sequenceOutput').textContent = fibArrayAsString;
 
-// console.log(guessResponse);
-var computer = Math.floor(Math.random()*100);
-console.log(computer);
-
-function game() {
-    var user = document.querySelector(".guess").value;
-    console.log(user);
-    
-           if (user > computer) {
-               document.getElementById("guessResponse").innerHTML = "<br> You're too high.";
-           } else if (user < computer) {
-               document.getElementById("guessResponse").innerHTML = "<br> You're too low.";
-           } else if (user == computer) {
-               document.getElementById("guessResponse").innerHTML = "<br> You're like a genius or something!!!!";
-           } else {
-               console.log("Yep");
-           }
 }
-
-// var max = 100;
-// var min = 1;
-
-// function getRandom(min,max) {
-//     return Math.random() * (max - min) + min;
-// }
-
-// var guessResponse;
-
-// if (guessResponse > getRandom) {
-//     document.write("<br> You're too high.");
-// } else if (guessResponse < getRandom) {
-     
-//         document.write("<br> You're too low.");
-        
-//     } else if (guessResponse === getRandom) {
-//         document.write("<br> You got it!");
-//     } else {
-//         document.write("<br> Choose a number");
-//     }
